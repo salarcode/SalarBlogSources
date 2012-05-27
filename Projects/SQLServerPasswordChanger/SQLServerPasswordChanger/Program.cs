8ef6cb6ec0888e4d441b2b3db09f5add173d3f7e
salar2k_cp <SND\salar2k_cp>
2012-05-27 10:39:12 +0000

    SQLServerPasswordChanger

---
+++ Projects/SQLServerPasswordChanger/SQLServerPasswordChanger/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.ServiceProcess;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SQLServerPasswordChanger
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main()
+		{
+			Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+
+			if (Environment.UserInteractive)
+			{
+				bool installed;
+				bool started;
+				CheckService("SQLServerPwd", out installed, out started);
+				if (installed)
+				{
+					if (MessageBox.Show("Do you want to uinstall the 'SQL Server Password Changer' service?", "SQLServerPwd", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+					{
+						try
+						{
+							UninstallService();
+						}
+						catch (Exception ex)
+						{
+							MessageBox.Show(ex.ToString(), "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+						}
+					}
+				}
+				else
+				{
+					if (MessageBox.Show("Do you want to install the 'SQL Server Password Changer' service?", "SQLServerPwd", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+					{
+						try
+						{
+							InstallService();
+						}
+						catch (Exception ex)
+						{
+							MessageBox.Show(ex.ToString(),"",MessageBoxButtons.OK,MessageBoxIcon.Error);
+						}
+					}
+				}
+			}
+			else
+			{
+				ServiceBase[] ServicesToRun;
+				ServicesToRun = new ServiceBase[] 
+				{ 
+					new TheService() 
+				};
+				ServiceBase.Run(ServicesToRun);
+			}
+		}
+
+		static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+		{
+			if(Environment.UserInteractive)
+			{
+				MessageBox.Show(e.Exception.ToString());
+			}
+			else
+			{
+				File.WriteAllText("lasterror.log",e.ToString());
+			}
+		}
+
+
+		/// <summary>
+		/// Strart stopped service
+		/// </summary>
+		public static void StartService(string svcName)
+		{
+			try
+			{
+				var svc = new ServiceController(svcName);
+				svc.Start();
+			}
+			catch (Exception)
+			{
+			}
+		}
+
+		public static void InstallService()
+		{
+			var ti = new TransactedInstaller();
+			ti.Installers.Add(new ProjectInstaller());
+			ti.Context = new InstallContext("", null);
+
+			string path = Application.ExecutablePath;
+			ti.Context.Parameters["assemblypath"] = path;
+
+			ti.Install(new Hashtable());
+		}
+
+		public static void UninstallService()
+		{
+			var ti = new TransactedInstaller();
+			ti.Installers.Add(new ProjectInstaller());
+			ti.Context = new InstallContext("", null);
+			string path = Application.ExecutablePath;
+
+			ti.Context.Parameters["assemblypath"] = path;
+			ti.Uninstall(null);
+		}
+
+
+
+		public static void CheckService(string svcName, out bool installed, out bool running)
+		{
+			installed = false;
+			running = false;
+
+			try
+			{
+				var svc = new ServiceController(svcName);
+				running = svc.Status == ServiceControllerStatus.Running;
+				installed = true;
+			}
+			catch (Exception)
+			{
+				// exception means not installed
+				installed = false;
+			}
+		}
+
+	}
+}
---
+++ Projects/SQLServerPasswordChanger/SQLServerPasswordChanger/TheService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SQLServerPasswordChanger
+{
+	public partial class TheService : ServiceBase
+	{
+		public TheService()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			var appPath = Path.GetDirectoryName(Application.ExecutablePath);
+			try
+			{
+				using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DbConnectionString"].ConnectionString))
+				{
+					conn.Open();
+					var sqlCommand = File.ReadAllText(Path.Combine(appPath, "cmd.sql"));
+					sqlCommand = ReplaceStrEx(sqlCommand, "GO", "\n;\n", StringComparison.InvariantCulture);
+
+					using (var cmd = conn.CreateCommand())
+					{
+						cmd.CommandText = sqlCommand;
+						int rows = cmd.ExecuteNonQuery();
+
+						File.WriteAllText(Path.Combine(appPath, "lasterror.log"), "Done, " + rows + " rows affected.");
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				File.WriteAllText(Path.Combine(appPath, "lasterror.log"), ex.ToString());
+			}
+			this.Stop();
+		}
+
+		protected override void OnStop()
+		{
+		}
+
+
+		/// <summary>
+		/// Implements fast string replacing algorithm for CS
+		/// </summary>
+		public static string ReplaceStrEx(string original, string pattern, string replacement, StringComparison comparisonType)
+		{
+			if (original == null)
+			{
+				return null;
+			}
+
+			if (String.IsNullOrEmpty(pattern))
+			{
+				return original;
+			}
+
+			int lenPattern = pattern.Length;
+			int idxPattern = -1;
+			int idxLast = 0;
+
+			StringBuilder result = new StringBuilder();
+			try
+			{
+				while (true)
+				{
+					idxPattern = original.IndexOf(pattern, idxPattern + 1, comparisonType);
+
+					if (idxPattern < 0)
+					{
+						result.Append(original, idxLast, original.Length - idxLast);
+
+						break;
+					}
+
+					result.Append(original, idxLast, idxPattern - idxLast);
+					result.Append(replacement);
+
+					idxLast = idxPattern + lenPattern;
+				}
+
+				return result.ToString();
+			}
+			finally
+			{
+				result.Length = 0;
+			}
+		}
+
+	}
+}
