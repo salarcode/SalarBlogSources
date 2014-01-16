e80f062bb7f192ace1ae492dc380a81ff6199f16
Salar Khalilzadeh <salar2k@gmail.com>
2014-01-16 06:25:11 +0000

    SqlServerConnectionTools is added.

--- Projects/Salar.HelperExtensions/Salar.HelperExtensions/MiscExtensions.cs
+++ Projects/Salar.HelperExtensions/Salar.HelperExtensions/MiscExtensions.cs
@@ -6,75 +6,78 @@ using System.Text;
 
 namespace System
 {
-	public static class MiscExtensions
-	{
-		/// <summary>
-		/// End time of the day
-		/// </summary>
-		public static DateTime ToEndTimeOfDay(this DateTime dateTime)
-		{
-			return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 23, 59, 59, 999);
-		}
+    public static class MiscExtensions
+    {
+        /// <summary>
+        /// End time of the day
+        /// </summary>
+        public static DateTime ToEndTimeOfDay(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 23, 59, 59, 999);
+        }
 
-		/// <summary>
-		/// Start time of the day
-		/// </summary>
-		public static DateTime ToStartTimeOfDay(this DateTime dateTime)
-		{
-			return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 0, 0, 0, 0);
-		}
+        /// <summary>
+        /// Start time of the day
+        /// </summary>
+        public static DateTime ToStartTimeOfDay(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 0, 0, 0, 0);
+        }
 
-		/// <summary>
-		/// Returns specified text in Description attribute.
-		/// </summary>
-		public static string GetDescription(this Enum value)
-		{
-			var fi = value.GetType().GetField(value.ToString());
-			var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
-			return attributes.Length > 0 ? attributes[0].Description : value.ToString();
-		}
+        /// <summary>
+        /// Returns specified text in Description attribute.
+        /// </summary>
+        public static string GetDescription(this Enum value)
+        {
+            var fi = value.GetType().GetField(value.ToString());
+            var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : value.ToString();
+        }
 
 
-		/// <summary>
-		/// Implements fast string replacing algorithm for CS
-		/// </summary>
-		public static string ReplaceAll(this string str, string pattern, string replacement, StringComparison comparisonType = StringComparison.CurrentCulture)
-		{
-			if (str == null)
-				throw new ArgumentNullException("str");
+        /// <summary>
+        /// Implements fast string replacing algorithm
+        /// </summary>
+        public static string ReplaceAll(this string str, string pattern, string replacement, StringComparison comparisonType = StringComparison.CurrentCulture)
+        {
+            if (str == null)
+                throw new ArgumentNullException("str");
 
-			if (String.IsNullOrEmpty(pattern))
-				return str;
+            if (String.IsNullOrEmpty(pattern))
+                return str;
 
-			int lenPattern = pattern.Length;
-			int idxPattern = -1;
-			int idxLast = 0;
+            if (replacement == null)
+                replacement = string.Empty;
 
-			var result = new StringBuilder();
-			try
-			{
-				while (true)
-				{
-					idxPattern = str.IndexOf(pattern, idxPattern + 1, comparisonType);
+            int lenPattern = pattern.Length;
+            int idxPattern = -1;
+            int idxLast = 0;
 
-					if (idxPattern < 0)
-					{
-						result.Append(str, idxLast, str.Length - idxLast);
-						break;
-					}
+            var result = new StringBuilder();
+            try
+            {
+                while (true)
+                {
+                    idxPattern = str.IndexOf(pattern, idxPattern + 1, comparisonType);
 
-					result.Append(str, idxLast, idxPattern - idxLast);
-					result.Append(replacement);
+                    if (idxPattern < 0)
+                    {
+                        result.Append(str, idxLast, str.Length - idxLast);
+                        break;
+                    }
 
-					idxLast = idxPattern + lenPattern;
-				}
-				return result.ToString();
-			}
-			finally
-			{
-				result.Length = 0;
-			}
-		}
+                    result.Append(str, idxLast, idxPattern - idxLast);
+                    result.Append(replacement);
 
-	}
+                    idxLast = idxPattern + lenPattern;
+                }
+                return result.ToString();
+            }
+            finally
+            {
+                result.Length = 0;
+            }
+        }
+
+    }
 }
